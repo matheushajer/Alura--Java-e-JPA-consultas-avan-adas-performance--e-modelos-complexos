69ebce1ff2602caa6ca63bde5c1a84c0ad289935
matheushajer <matheushajer@gmail.com>
2022-09-22 19:00:30 -0300

    Aula 3 - Consultas com Criteria API

--- loja_V2/src/main/java/br/com/matheushajer/loja_V2/dao/PedidoDAO.java
+++ loja_V2/src/main/java/br/com/matheushajer/loja_V2/dao/PedidoDAO.java
@@ -1,11 +1,17 @@
 package br.com.matheushajer.loja_V2.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.matheushajer.loja_V2.modelo.Pedido;
+import br.com.matheushajer.loja_V2.modelo.Produto;
 import br.com.matheushajer.loja_V2.vo.RelatorioDeVendasVo;
 
 public class PedidoDAO {
@@ -41,6 +47,29 @@ public class PedidoDAO {
 	public Pedido buscarPedidoComCliente(Long id) {
 		return em.createQuery("SELECT p FROM Pedido p JOIN FETCH p.cliente WHERE p.id =:id",Pedido.class)
 				.setParameter("id", id).getSingleResult();
+	}
+	
+	public List<Produto> BuscarPorParametrosCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+		
+		Root<Produto> from = query.from(Produto.class);
+		
+		Predicate filtros = builder.and();
+		if(nome != null && !nome.trim().isEmpty()) {
+			builder.and(filtros, builder.equal(from.get("nome"), nome));
+		}
+		if(preco != null) {
+			builder.and(filtros, builder.equal(from.get("preco"), preco));
+		}
+		if(dataCadastro !=null) {
+			builder.and(filtros, builder.equal(from.get("dataCadastro"), dataCadastro));
+		}
+
+		query.where(filtros);
+		
+		return em.createQuery(query).getResultList();
 	}
 	
 }
