ea8ee12e6a63e11841456b15408ba5f606e063af
matheushajer <matheushajer@gmail.com>
2022-09-22 19:14:12 -0300

    Aula 5 - Simplificando entidades com Embeddable

--- loja_V2/src/main/java/br/com/matheushajer/loja_V2/modelo/Cliente.java
+++ loja_V2/src/main/java/br/com/matheushajer/loja_V2/modelo/Cliente.java
@@ -1,5 +1,6 @@
 package br.com.matheushajer.loja_V2.modelo;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,15 +14,16 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private String cpf;
+	
+	@Embedded
+	private DadosPessoais dadosPessoais;
 
 	public Cliente() {
 	}
 
 	public Cliente(String nome, String cpf) {
-		this.nome = nome;
-		this.cpf = cpf;
+		this.dadosPessoais = new DadosPessoais(nome, cpf);
+
 	}
 
 	public Long getId() {
@@ -32,20 +34,8 @@ public class Cliente {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
+	public DadosPessoais getDadosPessoais() {
+		return dadosPessoais;
 	}
 
 }
