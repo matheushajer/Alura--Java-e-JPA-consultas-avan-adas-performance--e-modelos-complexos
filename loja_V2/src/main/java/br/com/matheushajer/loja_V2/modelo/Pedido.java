95821d9a49ab7c5647d12a748f1050cbf9895f16
matheushajer <matheushajer@gmail.com>
2022-09-22 18:22:16 -0300

    Aula 3 - Entendendo Lazy e Eager

--- loja_V2/src/main/java/br/com/matheushajer/loja_V2/modelo/Pedido.java
+++ loja_V2/src/main/java/br/com/matheushajer/loja_V2/modelo/Pedido.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,7 +30,7 @@ public class Pedido {
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens = new ArrayList<>();
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Cliente cliente;
 
 	public Pedido() {
